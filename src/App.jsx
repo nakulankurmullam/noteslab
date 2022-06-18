c29958754664eae82d1af78dd264179eb2f71d84
nakulankurmullam <nakulankurmullam66@gmail.com>
2022-06-18 23:19:52 +0530

    initiated firebase integration

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,25 @@ import Landing from "./Pages/Landing";
 import Student from "./Pages/Student";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
+import { UserAuthContextProvider } from "./Context/userAuthContext";
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/student-login" element={<Login />} />
-          <Route path="/faculty-login" element={<Login />} />
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/faculty" element={<Faculty />} />
-          <Route path="/student" element={<Student />} />
-        </Routes>
+        <UserAuthContextProvider>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/student-login" element={<Login />} />
+            <Route path="/faculty-login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/faculty" element={<Faculty />} />
+            <Route path="/student" element={<Student />} />
+          </Routes>
+        </UserAuthContextProvider>
       </Router>
-    </>)
+    </>
+  );
 }
 
 export default App;
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,28 +1,40 @@
 import React, { useState } from "react";
-import './Signup.css'
+import "./Signup.css";
 import { Link, useNavigate } from "react-router-dom";
 import { Form, Alert } from "react-bootstrap";
-import { Button } from "react-bootstrap";
+import { Button, InputGroup } from "react-bootstrap";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
+  const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [regNum, setRegNum] = useState("");
+  const [dept, setDept] = useState("");
+  const [isStudent, setIsStudent] = useState("");
+  const [sem, setSem] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!sem || !dept || !userName || !regNum) {
+      alert("Make sure to fill the form");
+      return;
+    }
     setError("");
     try {
       navigate("/");
     } catch (err) {
       setError(err.message);
-    } 
+    }
   };
 
   return (
     <>
       <div className=" box signup-container">
-        <h2 className="mb-3">Join Notes Lab <i className="fa-solid fa-flask"></i></h2>
+        <h2 className="mb-3">
+          Join Notes Lab <i className="fa-solid fa-flask"></i>
+        </h2>
         {error && <Alert variant="danger">{error}</Alert>}
         <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -30,6 +42,15 @@ const Signup = () => {
               type="email"
               placeholder="Email address"
               onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              type="text"
+              placeholder="User Name"
+              onChange={(e) => setUserName(e.target.value)}
+              required="required"
             />
           </Form.Group>
 
@@ -41,7 +62,102 @@ const Signup = () => {
             />
           </Form.Group>
 
-          <div className="d-grid gap-2">
+          <div className="signup-type mb-3">
+            <input
+              type="radio"
+              name="reg-type"
+              value="faculty"
+              id="fcltyType"
+              onChange={(e) => setIsStudent(false)}
+            />
+            <label htmlFor="fcltyType">Faculty</label>
+            <input
+              type="radio"
+              name="reg-type"
+              value="student"
+              id="stdntType"
+              checked="true"
+              onChange={(e) => setIsStudent(true)}
+            />
+            <label htmlFor="stdntType">Student</label>
+          </div>
+
+          {isStudent && (
+            <div className="stdnt-detail">
+              <Form.Select
+                onChange={(e) => setDept(e.target.value)}
+                className="mb-3"
+              >
+                <option>Choose Department</option>
+                <option value="CSE">CSE</option>
+                <option value="CE">CE</option>
+                <option value="ME">ME</option>
+                <option value="EEE">EEE</option>
+                <option value="ECE">ECE</option>
+              </Form.Select>
+              <Form.Select
+                onChange={(e) => setSem(e.target.value)}
+                className="mb-3"
+              >
+                <option>Choose Semester</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+              </Form.Select>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control
+                  type="text"
+                  placeholder="Register Number"
+                  onChange={(e) => setRegNum(e.target.value)}
+                  required="required"
+                />
+              </Form.Group>
+            </div>
+          )}
+          {!isStudent && (
+            <div className="stdnt-detail">
+              <Form.Select
+                onChange={(e) => setDept(e.target.value)}
+                className="mb-3"
+              >
+                <option>Choose Department</option>
+                <option value="CSE">CSE</option>
+                <option value="CE">CE</option>
+                <option value="ME">ME</option>
+                <option value="EEE">EEE</option>
+                <option value="ECE">ECE</option>
+              </Form.Select>
+              <Form.Select
+                onChange={(e) => setSem(e.target.value)}
+                className="mb-3"
+              >
+                <option>Choose Semester</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+              </Form.Select>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control
+                  type="text"
+                  placeholder="Register Number"
+                  onChange={(e) => setRegNum(e.target.value)}
+                  required="required"
+                />
+              </Form.Group>
+            </div>
+          )}
+
+          <div className="d-grid ">
             <Button variant="primary" id="signup-btn" type="Submit">
               Sign up
             </Button>
